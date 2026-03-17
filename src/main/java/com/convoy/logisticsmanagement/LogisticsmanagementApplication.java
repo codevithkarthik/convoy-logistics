dcb142e3ac404629d12595ede88d6c71af1b425f
Karthik <karthiktejakalluri@gmail.com>
2026-03-17 16:34:46 +0530

    Convoy Logistics Application

---
+++ src/main/java/com/convoy/logisticsmanagement/LogisticsmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.convoy.logisticsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsmanagementApplication.class, args);
+	}
+
+}
