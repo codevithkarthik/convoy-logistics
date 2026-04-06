2a25193f0922aeb9de940505f37329161685c170
Karthik <karthiktejakalluri@gmail.com>
2026-04-06 20:49:10 +0530

    Added UnloadingDto

---
+++ src/main/java/com/convoy/logisticsmanagement/dto/UnloadingDto.java
@@ -0,0 +1,34 @@
+package com.convoy.logisticsmanagement.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+public class UnloadingDto {
+	@NotNull
+	private String date;
+	@NotNull
+	private String time;
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public UnloadingDto(@NotNull String date, @NotNull String time) {
+		super();
+		this.date = date;
+		this.time = time;
+	}
+	public UnloadingDto() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "LoadingDto [date=" + date + ", time=" + time + "]";
+	}
+}
